b4b1bf545460bd8d3a431737c3bcde17589f2f06
lourivanrluz <lourivan-rodrigues@hotmail.com>
2022-05-02 15:11:45 -0300

    feature: add route home

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,8 +4,10 @@ from flask import Flask
 from app.routes.graph_router import bp_graph
 from app.routes.routes_router import bp_routes
 from app.routes.distance_router import bp_distance
+from app.routes.home_router import bp_home
 
 def init_app(app:Flask):
+    app.register_blueprint(bp_home)
     app.register_blueprint(bp_graph)
     app.register_blueprint(bp_routes)
     app.register_blueprint(bp_distance)
