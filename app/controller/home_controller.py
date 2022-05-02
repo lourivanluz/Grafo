ce1a93294ae57d6132c6c33631560a8ce2c4a2aa
lourivanrluz <lourivan-rodrigues@hotmail.com>
2022-05-02 14:40:35 -0300

    fix: remove home

--- app/controller/home_controller.py
+++
@@ -1,5 +0,0 @@
-from flask import redirect
-from http import HTTPStatus
-
-def home():
-    return redirect('https://gitlab.com/lourivanRluz/desafio-dev-jr-pl/-/blob/main/README.md'),HTTPStatus.SEE_OTHER
--- app/routes/home_router.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-
-from app.controller import home_controller
-
-bp_home = Blueprint('home',__name__,url_prefix='/')
-
-bp_home.get('')(home_controller.home)
